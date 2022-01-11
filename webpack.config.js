53f98d7cd041d8fa216591d6a7d439267b503ab6
Maxim <mr-4x@yandex.ru>
2022-01-11 15:14:05 +0300

    release v1

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HTMLPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    filename: "bundle.[chunkhash].js",
+    path: path.resolve(__dirname, "public"),
+  },
+  devServer: {
+    port: 3000,
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
+
+// import { resolve } from "path";
+
+// export const entry = "./src/app.js";
+// export const output = {
+//     filename: "bundle.js",
+//     path: resolve(__dirname, "public"),
+// };
+// export const devServer = {
+//     port: 3000,
+// };
